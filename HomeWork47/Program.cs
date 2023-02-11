e2eb998276a6b82f1747d95be19c88793f6e6967
Ekaterina <katya_usova@bk.ru>
2023-02-11 15:46:18 +0300

    HomeWork47

---
+++ HomeWork47/Program.cs
@@ -0,0 +1,28 @@
+﻿ class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random myRandom = new System.Random();
+            System.Console.WriteLine("m = 3");
+            System.Console.WriteLine("n = 4");
+            int m = 3, n = 4;
+            double[,] array = new double[m, n];
+
+            for(int i = 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    array[i, j] = myRandom.Next()/ 1392938;
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    System.Console.Write("{0,5:f1} ", array[i,j]);
+                }
+                System.Console.Write("\n");
+            }
+            System.Console.ReadKey();
+        }
+    }
